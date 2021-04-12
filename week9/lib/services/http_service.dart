75436cbdc2e80349a5737a33248b40858197896b
MochHelmiHasan6 <70736429+MochHelmiHasan6@users.noreply.github.com>
2021-04-12 13:29:16 +0700

    Membuat model untuk response HTTP

--- week9/lib/services/http_service.dart
+++ week9/lib/services/http_service.dart
@@ -1,5 +1,7 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:week9/models/movie.dart';
 
 class HttpService {
   final String apiKey = '1246932b429a348d0806a05f96328f67';
@@ -11,8 +13,10 @@ class HttpService {
     http.Response result = await http.get(Uri.parse(uri));
     if (result.statusCode == HttpStatus.ok) {
       print("Sukses");
-      String response = result.body;
-      return response;
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       print("Fail");
       return null;
